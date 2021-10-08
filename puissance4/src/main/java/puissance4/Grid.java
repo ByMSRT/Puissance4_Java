ed5222cd8e1523990c49decb5f89f292edeb5d7f
kevinguyodo <kevin.guyodo@ynov.com>
2021-10-08 20:30:33 +0200

    Version non fonctionnel

--- puissance4/src/main/java/puissance4/Grid.java
+++ puissance4/src/main/java/puissance4/Grid.java
@@ -29,7 +29,6 @@ public class Grid {
         }
     }
 
-
     public void gridCreation() {
         for (int y = 0; y < line; y++) {
             for (int x = 0; x < column; x++) {
@@ -52,32 +51,25 @@ public class Grid {
     //  ----------------------------------- Placement Pion ---------------------------------------- 
     
     public void nextToken(){
-        int tour = 0;
 
         do {
             Token tokenPlayer1 = new Token("1", 'X');
             validePlacement(tokenPlayer1);
-            horizontalVictory(tokenPlayer1.signToken);
-            verticalVictory(tokenPlayer1.signToken);
             Token tokenPlayer2 = new Token("2", 'O');
             validePlacement(tokenPlayer2);
-            horizontalVictory(tokenPlayer2.signToken);
-            verticalVictory(tokenPlayer2.signToken);
-            tour++;
-        } while (tour < 100); // fonction qui vérifie victoire null ou égalité)
+        } while (!placeInGrid()); // fonction qui vérifie si il reste de la place dans la grille.
     }
 
     public boolean placeToken(Token newToken) {
-
         for (int y = line-1; y >= 0; y--) {
             if (horizontalLine[y][newToken.userToken-1] == '-') {
                 horizontalLine[y][newToken.userToken-1] = newToken.signToken;
+                verificationVictory(newToken.signToken, y, newToken.userToken-1);
                 break;
-            }
+            } 
             if (y == 0) {
                 return false;
             }
-        
         }
         return true;
     }
@@ -90,30 +82,110 @@ public class Grid {
         }
     }
 
-    // --------------------------- Condition de victoire -------------------------------
-
-    public void horizontalVictory(char symbol) {
-        int victory = 1;
-
-        for(int y = line-1; y >= 0; y--) {
-            for (int x = 0; x < column-1; x++) {
-                if (horizontalLine[y][x] == symbol && horizontalLine[y][x+1] == symbol) {
-                    victory++;
-                }
+    public boolean placeInGrid() {
+        int count = 0;
+        for (int x = 0; x < column; x++) {
+            if (horizontalLine[1][x] == 'X' || horizontalLine[1][x] == 'O') {
+                count++;
             }
-        } 
-        System.out.println("Il y a " + victory + " aligne sur l'axe horizontal avec le symbol "+ symbol);
+        }   
+        System.out.println(count);
+        if (count < 7) {
+            return false;
+        } else {
+            return true;
+        }
+    
     }
 
-    public void verticalVictory(char symbol) {
-        int victory = 1;
-        for (int x = 0; x < column; x++) {
-            for (int y = 0; y < line-1; y++) {
-                if (horizontalLine[y][x] == symbol && horizontalLine[y+1][x] == symbol) {
-                    victory++;
-                }
-            }
+    // --------------------------- Condition de victoire -------------------------------
+
+    public void verificationVictory(char symbol, int y, int x) {
+        // boolean victory = false;
+        if (y == 0) {
+            // victory = true;
+            System.out.println();
+        } else if (horizontalVictory(symbol, y, x) == 4) {
+            // victory = true;
+            System.out.println("Victoire horizontal du joueur qui a le symbol " + symbol);
+        } else if (verticalVictory(symbol, y ,x) == 4) {
+            // victory = true;
+            System.out.println("Victoire vertical du joueur qui a le symbol " + symbol);
+        } else if (diagonalHD(symbol, y, x) == 4) {
+            // victory = true;
+            System.out.println("Victoire diagonale HD du joueur qui a le symbol " + symbol);
+        } else if (diagonalHG(symbol, y, x) == 4) {
+            // victory = true;
+            System.out.println("Victoire diagonale HG du joueur qui a le symbol " + symbol);
+        } 
+        // return victory;
+    }
+
+    // -------------------- Victoire horizontal ------------------------
+    public int horizontalVictory(char symbol, int y, int x) {
+        int victory = 0;
+        while (x < column-1 && horizontalLine[y][x] == symbol) {
+            x++;
+            victory++;
+        }
+        while (x >= 0 && horizontalLine[y][x] == symbol) {
+            x--;
+            victory++;
+        }
+
+        System.out.println("Il y a " + victory + " aligne sur l'axe horizontal en partant de la fin avec le symbol "+ symbol);
+        return victory;
+    }
+
+    // -------------------- Victoire vertical ------------------------
+
+    public int verticalVictory(char symbol, int y, int x) {
+        int victory = 0;
+        while (y < line-1 && horizontalLine[y][x] == symbol) {
+            y++;
+            victory++;
+        }
+        while (y >= 0 && horizontalLine[y][x] == symbol) {
+            y--;
+            victory++;
         }
         System.out.println("Il y a " + victory + " aligne sur l'axe vertical avec le symbol "+ symbol);
+        return victory;
+    }
+
+    // ---------------- Victoire diagonale en Haut-Droite -------------------
+
+    public int diagonalHD(char symbol, int y, int x){
+        int victory = 1;
+
+        while (x < column-1 && y >= 0 && horizontalLine[y][x] == symbol) {
+            x++; 
+            y--;
+            victory++;
+            continue;
+        }
+
+        while (x >= 0 && y < line-1 && horizontalLine[y][x] == symbol) {
+            x--;
+            y++;
+            victory++;
+            continue;
+        }
+
+        System.out.println("Il y a " + victory + " aligne en diagonale avec le symbol "+ symbol);
+        return victory;
+    }
+
+    // ---------------- Victoire diagonale en Haut-Gauche -------------------
+
+    public int diagonalHG(char symbol, int y, int x) {
+        int victory = 1;
+        while (x >= 0 && y >= 0 && horizontalLine[y][x] == symbol) {
+            x--;
+            y--; 
+            victory++;
+        }
+        System.out.println("Il y a " + victory + " aligne en diagonale avec le symbol "+ symbol);
+        return victory;
     }
 }

31c31f4b7b9a7bb696fa17ef780abe4c9e04e89e
ByMSRT <73824665+ByMSRT@users.noreply.github.com>
2021-10-12 23:19:44 +0200

    Update App.java

--- puissance4/src/main/java/puissance4/App.java
+++ puissance4/src/main/java/puissance4/App.java
@@ -1,96 +1,173 @@
 package puissance4;
 
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.Event;
 import javafx.geometry.Pos;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.effect.GaussianBlur;
+import javafx.scene.effect.Glow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Stop;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 // import javafx.stage.Window;
+import jdk.incubator.vector.VectorOperators.Test;
 
-public class App extends Application
-{
+public class App extends Application {
+    
+    private GameMenu gameMenu;
+    private GameMenu gameMenu2;
+    private Stage primaryStage = null;
 
-    private int nbrOfCliks = 0;
-
-    private Stage mainWindow = null;
-    private Stage confirmationStage = null;
-    private Scene mainScene = null;
-
-
-    public static void main( String[] args )
-    {
+    public static void main(String[] args) {
         launch(args);
     }
-
     
     @Override
-    public void start(Stage mainWindow) throws Exception {
-        // TODO Auto-generated method stub
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Puissance 4");
 
-        this.mainWindow = mainWindow;
+        Pane root = new Pane();
+        root.setPrefSize(800, 600);
+
+        /* InputStream is = Files.newInputStream(Paths.get("./puissance4.png"));
+        Image img = new Image(is);
+        is.close();
+
+        ImageView imgView = new ImageView(img); */
+
+        gameMenu = new GameMenu();
         
-        Button b = new Button("Click");
-        Label l = new Label("Hello");
 
+        root.getChildren().addAll(gameMenu);
 
-        b.setOnMouseClicked(e -> {
-            nbrOfCliks++;
-            l.setText("Clicked : " + nbrOfCliks);
+        Scene scene = new Scene(root);
 
-            Scene confirmationScene = createdConfirmationView();
-            confirmationStage = new Stage();
-            confirmationStage.setTitle("confirmation");
-            confirmationStage.setScene(confirmationScene);
-            confirmationStage.initModality(Modality.APPLICATION_MODAL);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
-            if(nbrOfCliks == 10) {
-                // mainWindow.setScene(confirmationScene);
-                confirmationStage.show();
-                nbrOfCliks = 0;
-            }
-        });
+    public void test() {
+
+        gameMenu2 = new GameMenu();
+        Pane root2 = new Pane();
+        root2.setPrefSize(800, 600);
+
+        root2.getChildren().addAll(gameMenu2);
+
+        Scene scene2 = new Scene(root2);
+
+        primaryStage.setScene(scene2);
+        primaryStage.show();
+
+    }
+
+    private class GameMenu extends Parent {
+        
+        public GameMenu(){
+            VBox menu0 = new VBox(10);
+
+            menu0.setTranslateX(270);
+            menu0.setTranslateY(250);
+
+            MenuButton btnPlay = new MenuButton("Play");
+            // Action
+            btnPlay.setOnMouseClicked(event -> {
+                GameMenu2();
+            });
+
+            MenuButton btnQuit = new MenuButton("Quit");
+            btnQuit.setOnMouseClicked(event -> {
+                Platform.exit();
+            });
+
+            menu0.getChildren().addAll(btnPlay, btnQuit);
+            getChildren().addAll(menu0);
+        }
+
+        public void GameMenu2(){
+            VBox menu1 = new VBox(10);
+
+            menu1.setTranslateX(270);
+            menu1.setTranslateY(250);
+
+            MenuButton btnPlay = new MenuButton("Local");
+            // Action
+            btnPlay.setOnMouseClicked(event -> {
+                
+            });
+
+            MenuButton btnOnline = new MenuButton("Online");
+            btnOnline.setOnMouseClicked(event -> {
+                
+            });
+
+            MenuButton btnQuit = new MenuButton("Quit");
+            btnQuit.setOnMouseClicked(event -> {
+                Platform.exit();
+            });
+
+            menu1.getChildren().addAll(btnPlay, btnOnline, btnQuit);
+            getChildren().addAll(menu1);
+        }
+    }
+
+    private static class MenuButton extends StackPane {
+        private Text text;
+
+        public MenuButton(String name) {
+            text = new Text(name);
+            text.setFont(text.getFont().font(20));
+            text.setFill(Color.WHITE);
+            Rectangle bg = new Rectangle(250,30);
+            bg.setOpacity(0.6);
+            bg.setFill(Color.BLACK);
+            bg.setEffect(new GaussianBlur(3.5));
+
             
-        
-        VBox box1 = new VBox();
-        box1.getChildren().addAll(l,b);
-        box1.setAlignment(Pos.CENTER); //pour centrer
-        box1.setSpacing(20);
- 
-        Scene mainScene = new Scene(box1, 300, 200);
-        this.mainScene = mainScene;
+            
+            setAlignment(Pos.CENTER);
+            setRotate(-0.5);
+            getChildren().addAll(bg, text);
+            setOnMouseEntered(event -> {
+                bg.setTranslateX(10);
+                text.setTranslateX(10);
+                bg.setFill(Color.WHITE);
+                text.setFill(Color.BLACK);
+            });
 
-        mainWindow.setTitle("My window");
-        mainWindow.setScene(mainScene);
+            setOnMouseExited(event -> {
+                bg.setTranslateX(0);
+                text.setTranslateX(0);
+                bg.setFill(Color.BLACK);
+                text.setFill(Color.WHITE);
+            });
 
-        mainWindow.show();
+            DropShadow drop = new DropShadow(50, Color.WHITE);
+            drop.setInput(new Glow());
+
+            setOnMousePressed(event -> setEffect(drop));
+            setOnMouseReleased(event -> setEffect(null));
+        }
+
     }
-
-    public Scene createdConfirmationView() {
-        Label l = new Label("You clicked 10 times, do you want to quit ?");
-        Button yes = new Button("Yes");
-        Button no = new Button("No");
-        HBox box2 = new HBox();
-        box2.getChildren().addAll(yes,no);
-        box2.setAlignment(Pos.CENTER);
-        box2.setSpacing(40);
-        VBox box1 = new VBox();
-        box1.getChildren().addAll(l, box2);
-        box1.setAlignment(Pos.CENTER);
-        box1.setSpacing(40);  
-        
-        yes.setOnMouseClicked(e -> {
-            this.mainWindow.close();
-            this.confirmationStage.close();
-        });
-
-        no.setOnMouseClicked(e -> {
-            this.confirmationStage.close();
-        });
-
-        return new Scene(box1, 200, 100);
-    }
+    
 }
